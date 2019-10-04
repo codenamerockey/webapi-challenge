05b38deed0c5a229c904321f29b4d6c4f75b9aab
Rockey Simmons <codenamerockey@gmail.com>
2019-10-04 10:35:51 -0700

    completed put endpoints, delete endpoints and conditionals

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -17,10 +17,56 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+  const postBody = req.body;
 
-router.put('/:id', (req, res) => {});
+  console.log(postBody);
+  dbAction
+    .insert(postBody)
+    .then(newAction => {
+      //   res.status(200).json(newAction);
+      if (!postBody.description || !postBody.notes) {
+        res.status(400).json({
+          message: 'please provide a description and notes for your action'
+        });
+      } else {
+        res.status(201).json(newAction);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'There was an error while saving the post to the database'
+      });
+    });
+});
 
-router.delete('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+  const projectId = req.params.id;
+  const changes = req.body;
+  dbAction
+    .update(projectId, changes)
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'There was an error while updating the post to the database'
+      });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  dbProject
+    .remove(req.params.id)
+    .then(() => {
+      res.status(204).json(req.params.id);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message:
+          'There was an error while deleting the project from the database'
+      });
+    });
+});
 
 module.exports = router;
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -40,10 +40,52 @@ router.get('/:id/actions', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+  dbProject
+    .insert(req.body)
+    .then(newProject => {
+      if (!req.body.name || !req.body.description) {
+        res.status(400).json({
+          message: 'Please provide a name and description for the project.'
+        });
+      } else {
+        res.status(201).json(newProject);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'There was an error while saving the post to the database'
+      });
+    });
+});
 
-router.put('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+  const projectId = req.params.id;
+  const changes = req.body;
+  dbProject
+    .update(projectId, changes)
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'There was an error while updating the post to the database'
+      });
+    });
+});
 
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+  dbProject
+    .remove(req.params.id)
+    .then(() => {
+      res.status(204).json(req.params.id);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message:
+          'There was an error while deleting the project from the database'
+      });
+    });
+});
 
 module.exports = router;
