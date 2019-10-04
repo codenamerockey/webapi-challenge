b26b881bf116308745a3a29d6c6e32fbf450fd96
Rockey Simmons <codenamerockey@gmail.com>
2019-10-04 08:21:20 -0700

    answered self study questions, and created the server with express, and added all dependencies

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+//Go code!
 
-Sing along:
+const express = require('express');
+const helmet = require('helmet');
 
-here's a little code I wrote, you might want to read it really slow, don't worry be happy
-in every line there may be trouble, but if you worry you make it double, don't worry, be happy
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, be happy
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, be happy
+const server = express();
 
-Go code!
-*/
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('<h1>Welcome to My Server</h1>');
+});
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => console.log(`\n**Server running on port ${port}`));
