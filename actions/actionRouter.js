65901fbe186ef0df5c4c1e3a29de4853b9ada710
Rockey Simmons <codenamerockey@gmail.com>
2019-10-04 09:10:12 -0700

    completed the get endpoints for projects and actions

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -4,7 +4,18 @@ const router = express.Router();
 
 const dbAction = require('../data/helpers/actionModel');
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  dbAction
+    .get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'error while retrieving actions from the database' });
+    });
+});
 
 router.post('/', (req, res) => {});
 
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -10,12 +10,35 @@ const dbAction = require('../data/helpers/actionModel');
 router.get('/', (req, res) => {
   dbProject
     .get()
-    .then()
-    .catch();
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'error while getting projects from the database' });
+    });
 });
 
 // return a list of actions for a project, use getProjectActions()
-router.get('/:id', (req, res) => {});
+router.get('/:id/actions', (req, res) => {
+  dbProject
+    .getProjectActions(req.params.id)
+    .then(actions => {
+      if (actions.length > 0) {
+        res.status(200).json(actions);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'action with specified id does not exist' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'Actions information could not be retrieved' });
+    });
+});
 
 router.post('/', (req, res) => {});
 
